7e76de2abaf6ca219fe1095385d6ba977ad6e12c
unknown <trongphuc5667@gmail.com>
2022-08-15 12:37:42 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    URL_API: "https://learning-web-education.herokuapp.com",
+    URL_API: "https://web-learning-back.vercel.app",
   },
 }
 
